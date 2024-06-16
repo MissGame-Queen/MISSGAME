3803636d9b2af4546e2c5ab8d131c9f83f5bf9ab
lygfyby <lygfyby@gmail.com>
2024-06-16 14:34:05 +0800

    Z13_20240616_1

--- src/task.h
+++ src/task.h
@@ -1,10 +1,15 @@
 #ifndef TASK_H
 #define TASK_H
 #include "main.h"
+#include <Adafruit_MCP23X17.h>
 #include <Audio.h>
+#include <DFRobotDFPlayerMini.h>
 extern QueueHandle_t queueBasketball;
+extern QueueHandle_t queuePCM5102;
+
 extern const uint8_t pinDS, pinSH, pinST;
 void Basketball(void *pvParam);
 void FlyingShip(void *pvParam);
 void set74HC595(String newTime);
+void taskPCM5102(void *pvParam);
 #endif
